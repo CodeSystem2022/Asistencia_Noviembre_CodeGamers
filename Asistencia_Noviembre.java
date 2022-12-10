608959edc304813b200c7d1fd5f2ef175d9cfa8e
Diego Di Stefano <92904868+diegodistefano@users.noreply.github.com>
2022-12-10 13:25:24 -0300

    Update Asistencia_Noviembre.java

--- Asistencia_Noviembre.java
+++ Asistencia_Noviembre.java
@@ -28,6 +28,42 @@ public class Arreglos_Ejercicio_1 {
 }
 
 
+
+
+/*
+Autor: Di Stefano Diego
+
+Ejercicio 1: Leer 5 numeros, guardandolos en un arreglo y 
+mostrarlos en el orden inverso al introducido
+*/
+
+package arreglos_ejercicio_2;
+
+import java.util.Scanner;
+
+public class Arreglos_Ejercicio_2 {
+    
+        public static void main(String[] args) {
+            Scanner entrada = new Scanner(System.in);
+            float[] numeros = new float[5];
+
+            System.out.println("Guardando los datos en el arreglo");
+            for (int i=0; i<5; i++) {
+                System.out.println((i+1)+".Digite un numero: ");
+                numeros[i] = entrada.nextFloat();
+            }
+        
+        System.out.println("\nImprimimos los elementos del arreglo");
+            for(int i=4; i>=4; i--) {
+                System.out.println(i+" ");
+        }
+        System.out.println("\n");
+    }
+}
+
+
+
+
 /*
 Autor: Gastón Exequiel Garay
 
