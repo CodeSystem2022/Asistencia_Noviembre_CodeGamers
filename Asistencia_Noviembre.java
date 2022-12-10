6752cf8a32a435508561e987fc1c16b464dbe4c6
maradiste <112035570+maradiste@users.noreply.github.com>
2022-12-10 17:51:59 -0300

    Update Asistencia_Noviembre.java

--- Asistencia_Noviembre.java
+++ Asistencia_Noviembre.java
@@ -120,3 +120,46 @@ public class Arreglos_Ejercicio_3 {
         //ingresar:2, -5, 6, 2, 0
     }
 }
+
+
+
+/*
+Autora: Mara Di Stefano
+
+Ejercicio 2: 
+*/
+
+package MatricesEjercicios;
+
+import java.util.Scanner;
+
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int matriz[][] = new int [7][7];
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i==j) {
+                    matriz[i][j] = 1;
+                }
+                else{
+                    matriz[i][j] =0;
+                }
+            }
+        }
+
+        System.out.println("Matriz");
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.println(matriz[i][j]+" ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        
+        
+    }
+}
+
